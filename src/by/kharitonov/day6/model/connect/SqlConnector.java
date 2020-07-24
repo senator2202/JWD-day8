720870695c572b516ba8063659e74285b36785a6
senator2202 <Nuttertools220291>
2020-07-25 00:23:29 +0300

    coding task

--- src/by/kharitonov/day6/model/connect/SqlConnector.java
+++ src/by/kharitonov/day6/model/connect/SqlConnector.java
@@ -1,25 +1,38 @@
 package by.kharitonov.day6.model.connect;
 
+import by.kharitonov.day6.model.exception.DaoException;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Properties;
 
 public class SqlConnector {
-    private static final String USER_NAME = "root";
-    private static final String USER_PASSWORD = "admin";
     private static final String HOST =
-            "jdbc:mysql://localhost:3306/book_warehouse" +
-                    "?verifyServerCertificate=false" +
-                    "&useSSL=false" +
-                    "&requireSSL=false" +
-                    "&useLegacyDatetimeCode=false" +
-                    "&amp" +
-                    "&serverTimezone=UTC";
+            "jdbc:mysql://localhost:3306/book_warehouse";
+    private static final Properties PROPERTIES;
+
+    static {
+        PROPERTIES = new Properties();
+        PROPERTIES.put("user", "root");
+        PROPERTIES.put("password", "admin");
+        PROPERTIES.put("autoreconnect", "true");
+        PROPERTIES.put("characterEncoding", "UTF-8");
+        PROPERTIES.put("useUnicode", "true");
+        PROPERTIES.put("serverTimezone", "UTC");
+        PROPERTIES.put("verifyServerCertificate", "false");
+        PROPERTIES.put("useSSL", "false");
+        PROPERTIES.put("requireSSL", "false");
+    }
 
     private SqlConnector() {
     }
 
-    public static Connection connect() throws SQLException {
-        return DriverManager.getConnection(HOST, USER_NAME, USER_PASSWORD);
+    public static Connection connect() throws DaoException {
+        try {
+            return DriverManager.getConnection(HOST, PROPERTIES);
+        } catch (SQLException e) {
+            throw new DaoException("Unable to get connection!");
+        }
     }
 }
